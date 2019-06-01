35dbf5b1f7ed5b2d7f2c4c014a19ae725d950fbd
as3379 <as3379@njit.edu>
2019-06-01 13:10:47 -0400

    Initial commit

---
+++ Basics/FireFoxTest.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+class Test():
+    def Testmethod(self):
+        driver = webdriver.Firefox(
+                 executable_path="C:\\Users\\adiwakar\\Documents\\Selenium Browser files\\Library\\geckodriver.exe")
+        driver.get("https://google.com")
+TT=Test()
+TT.Testmethod()
